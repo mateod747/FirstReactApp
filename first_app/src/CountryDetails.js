a4dde8d0a224b3057b54a1ee867b0d4c2b7eb86a
mateod747 <mateod747@gmail.com>
2021-03-03 15:53:02 +0100

    da

--- first_app/src/CountryDetails.js
+++ first_app/src/CountryDetails.js
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function CountryDetails({ match }) {
+function CountryDetails( ) {
     useEffect(() => {
         fetchItem();
-        console.log(match);
     }, []);
 
-    const [ item, setItem ] = useState([]);
+    const [ items, setItems ] = useState([]);
+
+    const json_body = {
+        LeagueSeasonId: "d9673ce8-4d7b-eb11-b566-0050f2ed1c57",
+        orderBy: "MatchDate%20asc",
+        IsPlayed: "false",
+        PageNumber: "1"
+    };
+
+    //League season Id = d9673ce8-4d7b-eb11-b566-0050f2ed1c57
+    //Match Id = 30b47b2b-5f7b-eb11-b566-0050f2ed1c57
 
     const fetchItem = async () => {
         const data = await fetch(
-            `https://restcountries.eu/rest/v2/name/${match.params.name}`
+            `https://localhost:44386/api/Match/Get?LeagueSeasonID=${json_body.LeagueSeasonId}&orderBy=${json_body.orderBy}`,             
         );
 
-        const item = await data.json();
-        console.log(item);
-        setItem(item);
+        const items = await data.json();
+        console.log(items);
+        setItems(items);
     };
 
     return (
         <div className="CountryDetails">
-            {item.map(item => (
-                <ul className="list">
-                    <li>Country: {item.name}</li>
-                    <li>Capital: {item.capital}</li>
-                    <li>Population: {item.population}</li>
-                </ul>
-            ))}
+                {items.map(item => (
+                    <ul className="list">
+                        <li>Home Team: {item.HomeTeam}</li>
+                        <li>Away Team: {item.AwayTeam}</li>
+                        <li>Match Date: {item.MatchDate} </li>
+                        <li>Referee Name: {item.RefereeName}</li>
+                    </ul>
+                ))}
         </div>
     );
 }
